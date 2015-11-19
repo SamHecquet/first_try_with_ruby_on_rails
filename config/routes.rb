1f78aae913141db99730c54374f1f89f7e0cd89d
SamHecquet <samhecquet@gmail.com>
2015-11-19 21:24:04 +0000

    add pagination

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,11 @@ Rails.application.routes.draw do
 
   get '/home', to: 'pages#home'
   
-  # recipes routes
-  #get '/recipes', to: 'recipes#index'
-  #get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
-  #post '/recipes', to: 'recipes#create'
-  #get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
-  #patch '/recipes/:id', to: 'recipes#update'  
-  #get '/recipes/:id', to: 'recipes#show', as: 'recipe'
-  #delete '/recipes/:id', to: 'recipes#destroy'
-
-  resources :recipes
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+  
+  
 end
